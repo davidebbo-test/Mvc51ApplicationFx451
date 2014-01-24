b09faa616c8890550bddecd7022b1a5e28b14baf
davidebbo <david.ebbo@microsoft.com>
2014-01-24 10:46:55 -0800

    Initial commit

---
+++ Mvc51ApplicationFx451/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc51ApplicationFx451.Startup))]
+namespace Mvc51ApplicationFx451
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
