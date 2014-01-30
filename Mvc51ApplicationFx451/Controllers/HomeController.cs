59b0a9020a8d0a8bfb4c3c06fc1a3f008aae7536
davidebbo <david.ebbo@microsoft.com>
2014-01-29 17:38:43 -0800

    Adding some trace info

--- Mvc51ApplicationFx451/Controllers/HomeController.cs
+++ Mvc51ApplicationFx451/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,10 @@ namespace Mvc51ApplicationFx451.Controllers
     {
         public ActionResult Index()
         {
+            Trace.TraceInformation("This is an informational trace message");
+            Trace.TraceWarning("Here is trace warning");
+            Trace.TraceError("Something is broken; tracing an error!");
+
             return View();
         }
 
